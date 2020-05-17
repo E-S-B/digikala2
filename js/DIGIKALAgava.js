a1a585852c12ff0cd38351687349d1b6f33e4eff
Elham Sadat Banaei <sadat.banaei2019@gmail.com>
2020-05-18 04:26:39 +0430

    design navbar in digikala

---
+++ js/DIGIKALAgava.js
@@ -0,0 +1,11 @@
+function clickmenu(x) {
+    x.classList.toggle('change')
+}
+
+function clickmenu2(x){
+    x.classList.toggle('change2')
+}
+
+function clickmenu3(x) {
+    x.classList.toggle('change3')
+}
