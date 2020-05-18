88ac266265d6e71202903720e05cd79465e1f621
Elham Sadat Banaei <sadat.banaei2019@gmail.com>
2020-05-18 13:37:29 +0430

    delete HTML and CSS file  and start again project

--- js/DIGIKALAgava.js
+++ js/DIGIKALAgava.js
@@ -1,11 +1,3 @@
 function clickmenu(x) {
     x.classList.toggle('change')
 }
-
-function clickmenu2(x){
-    x.classList.toggle('change2')
-}
-
-function clickmenu3(x) {
-    x.classList.toggle('change3')
-}
