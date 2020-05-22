f0ae968f74a27773e09e0af5297e5c6cdcbb82ad
Elham Sadat Banaei <sadat.banaei2019@gmail.com>
2020-05-22 22:00:20 +0430

    edit navbar

--- js/DIGIKALAgava.js
+++ js/DIGIKALAgava.js
@@ -1,3 +1,36 @@
-function clickmenu(x) {
-    x.classList.toggle('change')
-}
+$(document).ready(() => {
+    $(".menu > li").on("mouseover", (event) => {
+        let right = 15;
+        let width = 0;
+        let that = event.target
+        $(".menu > li:not(.hover)").each((index, item) => {
+            if(that == item) {
+                width = $(item).css("width");
+                return false;
+            }
+            else right += parseInt($(item).css("width"), 10);
+        });
+        $(".menu > li.hover").css({
+            right: right,
+            width: width,
+            transform: "scaleX(1)"
+        });
+    });
+    $(".menu > li").on("mouseout", () => {
+        $(".menu > li.hover").css("transform", "scaleX(0)")
+    })
+    new Swiper ('.swiper-container', {
+        direction: 'horizontal',
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev'
+        },
+        scrollbar: {el: '.swiper-scrollbar'},
+        autoplay: {delay: 5000}
+    })
+});
